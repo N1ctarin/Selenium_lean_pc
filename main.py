6644d1a0c3c1fd8efa93cb94177e063ee59317f3
Иван_Home <kostenkovan@yandex.ru>
2024-10-25 22:53:55 +0300

    New les_1

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-if __name__ == '__main__':
-    print_hi('PyCharm')
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/simple_form_find_task.html")
+button = browser.find_element(By.ID, "submit_button")
