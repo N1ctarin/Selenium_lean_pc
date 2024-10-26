c0eb4c831549c74da9acc821da1af71884fa4e19
Иван_Home <kostenkovan@yandex.ru>
2024-10-26 14:24:28 +0300

    New les_2_4

--- lessons/lesson1_4.py
+++ lessons/lesson1_4.py
@@ -8,11 +8,11 @@ try:
     browser = webdriver.Chrome()
     browser.get(link)
 
-    input1 = browser.find_element(By.TAG_NAME, "first_name")
+    input1 = browser.find_element(By.TAG_NAME, "input")
     input1.send_keys("Ivan")
     input2 = browser.find_element(By.NAME, "last_name")
     input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, "firstname")
+    input3 = browser.find_element(By.CLASS_NAME, "city")
     input3.send_keys("Smolensk")
     input4 = browser.find_element(By.ID, "country")
     input4.send_keys("Russia")
