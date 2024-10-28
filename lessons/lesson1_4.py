141089c64131fc56c11952939fb9cddf947ce323
EKF <kostenkovan@yandex.ru>
2024-10-28 09:29:03 +0300

    Update

--- lessons/lesson1_4.py
+++ lessons/lesson1_4.py
@@ -1,30 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import math
-
-link = "http://suninjuly.github.io/find_link_text"
 
 try:
+    link = "http://suninjuly.github.io/registration1.html"
     browser = webdriver.Chrome()
     browser.get(link)
 
-    text = browser.find_element(By.PARTIAL_LINK_TEXT, str(math.ceil(math.pow(math.pi, math.e)*10000)))
-    text.click()
-    input1 = browser.find_element(By.TAG_NAME, "input")
-    input1.send_keys("Ivan")
-    input2 = browser.find_element(By.NAME, "last_name")
-    input2.send_keys("Petrov")
-    input3 = browser.find_element(By.CLASS_NAME, "city")
-    input3.send_keys("Smolensk")
-    input4 = browser.find_element(By.ID, "country")
-    input4.send_keys("Russia")
+    # Ваш код, который заполняет обязательные поля
+    input_1 = browser.find_element(By.CLASS_NAME, "first")
+    input_1.send_keys("name")
+    input_2 = browser.find_element(By.CLASS_NAME, "second")
+    input_2.send_keys("last_name")
+    input_3 = browser.find_element(By.CLASS_NAME, "third")
+    input_3.send_keys("name@gmail.com")
+
+    # Отправляем заполненную форму
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(1)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
 finally:
-    # успеваем скопировать код за 30 секунд
-    time.sleep(30)
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
     # закрываем браузер после всех манипуляций
     browser.quit()
-
