b720a1cbcc907c5f5f145d182fc58b9378dc751d
EKF <kostenkovan@yandex.ru>
2024-10-29 11:14:02 +0300

    Update

--- lessons/lesson_2.py
+++ lessons/lesson_2.py
@@ -4,15 +4,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import math
 
+from selenium.webdriver.common.devtools.v85.dom import get_attributes
+
+
 def calc(x):
   return str(math.log(abs(12*math.sin(int(x)))))
 
-link = "https://suninjuly.github.io/math.html"
+link = "https://suninjuly.github.io/get_attribute.html"
 browser = webdriver.Chrome()
 browser.get(link)
 
-x_element = browser.find_element(By.ID, "input_value")
-x = x_element.text
+x_element = browser.find_element(By.ID, "treasure")
+x = x_element.get_attribute("valuex")
 y = calc(x)
 
 pole = browser.find_element(By.ID, "answer")
