923fbaba161f608fad44f42b539ead10a27808a9
EKF <kostenkovan@yandex.ru>
2024-11-01 15:33:06 +0300

    Update

--- lessons/lesson_8.py
+++ lessons/lesson_8.py
@@ -1,7 +1,4 @@
-from time import sleep
-
 from selenium import webdriver
-from selenium.webdriver.support.expected_conditions import text_to_be_present_in_element
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ES
