3ae1aeeddeed1b882fb08e0a8425bfbcb6c83076
EKF <kostenkovan@yandex.ru>
2024-11-08 10:49:39 +0300

    Update

--- stepik/test_auturiz.py
+++ stepik/test_auturiz.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.by import By
 import json
 import math
 
-from lessons.lesson_1 import button
 
-answer = math.log(int(time.time()))
+
 
 @pytest.fixture(scope="session")
 def load_config():
@@ -21,21 +20,24 @@ class TestLogin:
         password_stepik = load_config['password']
 
         link = "https://stepik.org/lesson/236895/step/1"
+
         browser.get(link)
+        browser.implicitly_wait(5)
+
         link_1 = browser.find_element(By.ID, "ember457")
         link_1.click()
-        time.sleep(3)
         email = browser.find_element(By.ID, 'id_login_email')
         email.send_keys(login_stepik)
         password = browser.find_element(By.ID, 'id_login_password')
         password.send_keys(password_stepik)
         button_1 = browser.find_element(By.CLASS_NAME, 'button_with-loader')
         button_1.click()
-        time.sleep(2)
+
+        answer = math.log(int(time.time()))
 
         pole_input = browser.find_element(By.TAG_NAME, 'textarea')
         pole_input.send_keys(answer)
         button_2 = browser.find_element(By.CLASS_NAME, 'submit-submission')
         button_2.click()
-        time.sleep(10)
+        time.sleep(5)
 
