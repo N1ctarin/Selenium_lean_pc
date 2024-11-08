77d76c286d70715ad15ac49c2916484fd1515a22
EKF <kostenkovan@yandex.ru>
2024-11-08 10:10:43 +0300

    Update

--- stepik/test_auturiz.py
+++ stepik/test_auturiz.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 
+
 @pytest.fixture(scope="session")
 def load_config():
     with open('package.json', 'r') as config_file:
@@ -15,15 +16,17 @@ class TestLogin:
         login_stepik = load_config['email']
         password_stepik = load_config['password']
 
-        link = "https://stepik.org/catalog?auth=login"
+        link = "https://stepik.org/lesson/236895/step/1"
         browser.get(link)
+        link_1 = browser.find_element(By.ID, "ember457")
+        link_1.click()
+        time.sleep(3)
         email = browser.find_element(By.ID, 'id_login_email')
         email.send_keys(login_stepik)
         password = browser.find_element(By.ID, 'id_login_password')
         password.send_keys(password_stepik)
-        button_1 = browser.find_element(By.TAG_NAME, 'Войти')
+        button_1 = browser.find_element(By.CLASS_NAME, 'button_with-loader')
         button_1.click()
-        time.sleep(3)
+        time.sleep(5)
 
-        browser.get("https://stepik.org/lesson/236895/step/1")
 
