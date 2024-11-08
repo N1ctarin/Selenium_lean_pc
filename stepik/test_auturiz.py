81555b5cff539ec814469996fbeadb5721f77540
EKF <kostenkovan@yandex.ru>
2024-11-08 13:08:32 +0300

    Update

--- stepik/test_auturiz.py
+++ stepik/test_auturiz.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 import json
 import math
 
-
+from main import browser
 
 
 @pytest.fixture(scope="session")
@@ -26,20 +26,21 @@ class TestLogin:
         browser.get(link)
         browser.implicitly_wait(5)
 
-        link_1 = browser.find_element(By.ID, "ember457")
+        link_1 = browser.find_element(By.CSS_SELECTOR, ".navbar__auth_login")
         link_1.click()
         email = browser.find_element(By.ID, 'id_login_email')
         email.send_keys(login_stepik)
         password = browser.find_element(By.ID, 'id_login_password')
         password.send_keys(password_stepik)
-        button_1 = browser.find_element(By.CLASS_NAME, 'button_with-loader')
+        button_1 = browser.find_element(By.CSS_SELECTOR, "button_with-loader")
         button_1.click()
-
+        time.sleep(4)
         answer = math.log(int(time.time()))
 
-        pole_input = browser.find_element(By.TAG_NAME, 'textarea')
+        pole_input = browser.find_element(By.CSS_SELECTOR, ".textarea")
+        pole_input.click()
         pole_input.send_keys(answer)
-        button_2 = browser.find_element(By.CLASS_NAME, 'submit-submission')
+        button_2 = browser.find_element(By.TAG_NAME, "button")
         button_2.click()
         time.sleep(5)
 
