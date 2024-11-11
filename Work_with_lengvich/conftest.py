f23a76a491695b192e21d00112b4b431725fc5ff
EKF <kostenkovan@yandex.ru>
2024-11-11 15:28:11 +0300

    Update

--- Work_with_lengvich/conftest.py
+++ Work_with_lengvich/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
-                     help="Choose browser: es or ru")
+                     help="Choose browser: es or fr")
 
 
 @pytest.fixture(scope="function")
@@ -13,8 +13,8 @@ def browser(request):
     if browser_language == "es":
         print("\nstart es browser for test..")
         browser = webdriver.Chrome()
-    elif browser_language == "ru":
-        print("\nstart ru browser for test..")
+    elif browser_language == "fr":
+        print("\nstart fr browser for test..")
         browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
