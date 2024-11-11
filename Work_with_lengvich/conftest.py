3319ac8979d7ec6d627b54e2296d78eeaaa42e81
EKF <kostenkovan@yandex.ru>
2024-11-11 14:52:23 +0300

    Update

--- Work_with_lengvich/conftest.py
+++ Work_with_lengvich/conftest.py
@@ -2,19 +2,19 @@ import pytest
 from selenium import webdriver
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
-                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default=None,
+                     help="Choose browser: es or ru")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
-    browser_name = request.config.getoption("browser_name")
+    browser_language = request.config.getoption("language")
     browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
+    if browser_language == "es":
+        print("\nstart es browser for test..")
         browser = webdriver.Chrome()
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
+    elif browser_language == "ru":
+        print("\nstart ru browser for test..")
         browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
---
+++ Work_with_lengvich/test_items.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.CSS_SELECTOR, "#login_link")
+    button_1 = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket").text
+
+    assert button_1 == "Добавить в корзину"
