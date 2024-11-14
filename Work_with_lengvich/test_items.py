4e2446459dd7907d90ae7c6618b7fc9c392753c3
EKF <kostenkovan@yandex.ru>
2024-11-14 15:02:45 +0300

    Добавил assert

--- Work_with_lengvich/test_items.py
+++ Work_with_lengvich/test_items.py
@@ -8,4 +8,4 @@ def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
     button_1 = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
-
+    assert button_1.is_displayed()
